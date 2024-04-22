4750700129a2bb9ca498738bb92eedb4ba764004
Dimersion32 <136644458+Dimersion32@users.noreply.github.com>
2024-04-23 00:32:04 +0300

    Create admin.py
    Create base extensoin

---
+++ cogs/admin.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+
+class AdminCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        """Kick a member from the server."""
+        await member.kick(reason=reason)
+        await ctx.send(f'{member.mention} has been kicked.')
+
+def setup(bot):
+    bot.add_cog(AdminCommands(bot))
